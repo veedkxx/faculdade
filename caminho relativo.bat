8c429c9af197529cae2f380d5ba39014e538965a
veedkxx <127156986+veedkxx@users.noreply.github.com>
2023-03-31 15:28:36 -0300

    Add files via upload

---
+++ caminho relativo.bat
@@ -0,0 +1,53 @@
+mkdir professor
+cd professor
+mkdir disciplina1 disciplina2 disciplina3 disciplina4 disciplina5
+cd disciplina1
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1 
+mkdir at
+cd ..\unidade2
+mkdir at
+cd ..\unidade3
+mkdir at
+cd ..\avs
+mkdir av1 av2 av3 recupera
+cd ..\..\disciplina2
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1 
+mkdir at
+cd ..\unidade2
+mkdir at
+cd ..\unidade3
+mkdir at
+cd ..\avs
+mkdir av1 av2 av3 recupera
+cd ..\..\disciplina3
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1 
+mkdir at
+cd ..\unidade2
+mkdir at
+cd ..\unidade3
+mkdir at
+cd ..\avs
+mkdir av1 av2 av3 recupera
+cd ..\..\disciplina4
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1 
+mkdir at
+cd ..\unidade2
+mkdir at
+cd ..\unidade3
+mkdir at
+cd ..\avs
+mkdir av1 av2 av3 recupera
+cd ..\..\disciplina5
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1 
+mkdir at
+cd ..\unidade2
+mkdir at
+cd ..\unidade3
+mkdir at
+cd ..\avs
+mkdir av1 av2 av3 recupera
